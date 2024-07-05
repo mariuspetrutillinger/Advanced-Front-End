d927502ca51b28641c8e7e87979cdf0ca1ade2ad
Marius Petru Tillinger <74731592+mariuspetrutillinger@users.noreply.github.com>
2024-07-05 11:58:15 +0300

    Update FirebaseConfig.ts

--- life-dot-me/src/backend/FirebaseConfig.ts
+++ life-dot-me/src/backend/FirebaseConfig.ts
@@ -6,16 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAS43RTlm_iTbslFqlKCID9KYxYBA6ObOk",
-  authDomain: "lifedotme-28a11.firebaseapp.com",
-  projectId: "lifedotme-28a11",
-  storageBucket: "lifedotme-28a11.appspot.com",
-  messagingSenderId: "1087730355821",
-  appId: "1:1087730355821:web:5e3f44e95293673bd1f964",
-  measurementId: "G-82S9J5Z7PJ",
-  databaseURL: "https://lifedotme-28a11-default-rtdb.europe-west1.firebasedatabase.app"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
